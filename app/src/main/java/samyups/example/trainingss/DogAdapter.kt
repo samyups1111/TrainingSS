dcdd931283ccacdd39b0a192835c8809a84f2f1a
Sammuel De Jesus <sdejesus2010@gmail.com>
2021-03-05 15:34:48 -0500

    Update main.xml

---
+++ app/src/main/java/samyups/example/trainingss/DogAdapter.kt
@@ -0,0 +1,12 @@
+package samyups.example.trainingss
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.ListAdapter
+
+class DogAdapter: ListAdapter<DogAdapter.DogViewHolder>() {
+
+    class DogViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+}
