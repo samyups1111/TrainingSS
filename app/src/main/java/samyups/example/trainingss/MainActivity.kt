d5a8842b68db4d7df0b995719b6439d1a54850d8
Sammuel De Jesus <sdejesus2010@gmail.com>
2021-03-09 15:14:04 -0500

    Remove empty lines. Fix RecyclerView items' size

--- app/src/main/java/samyups/example/trainingss/MainActivity.kt
+++ app/src/main/java/samyups/example/trainingss/MainActivity.kt
@@ -3,26 +3,20 @@ package samyups.example.trainingss
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    val dogAdapter = DogAdapter()
-    private var layoutManager: RecyclerView.LayoutManager? = null
+    private val dogAdapter = DogAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        initDogRV()
+        initRecyclerView()
     }
-
-    private fun initDogRV() {
-        dogRV.apply {
+    private fun initRecyclerView() {
+        main_recyclerview.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
             adapter = dogAdapter
         }
-
     }
 }
